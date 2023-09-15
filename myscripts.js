12c32ed74fcf01ee97cab661fa0ca2f01ea8ddf5
Minato Arisato <zlaumari022@yahoo.com>
2023-09-16 02:34:43 +0800

    edited

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,19 @@
-// Get a reference to the audio element
-const audio = document.getElementById("myAudio");
+// JavaScript code to hide the audio player after playing the song
+const audioPlayer = document.getElementById("audio-player");
+const hideAudioButton = document.getElementById("hide-audio-button");
 
-// Add a click event listener to toggle the visibility
-audio.addEventListener("click", function() {
-  if (audio.style.display === "none" || audio.style.display === "") {
-    audio.style.display = "block";
-  } else {
-    audio.style.display = "none";
-  }
+// Add an event listener to the audio player to detect when it finishes playing
+audioPlayer.addEventListener("ended", () => {
+    // Hide the audio player
+    audioPlayer.style.display = "none";
+    // Show the "Hide Player" button
+    hideAudioButton.style.display = "block";
+});
+
+// Add an event listener to the "Hide Player" button to show the audio player again
+hideAudioButton.addEventListener("click", () => {
+    // Show the audio player
+    audioPlayer.style.display = "block";
+    // Hide the "Hide Player" button
+    hideAudioButton.style.display = "none";
 });
